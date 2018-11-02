279c6d8e85acc01cbf6b41daab943130f92fe2e6
Aleksbgbg <kozichka01@abv.bg>
2018-11-02 09:13:24 +0000

    [PartyInvites] Complete first iteration of project with basic layouts, and complete models and controllers.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,8 +1,43 @@
 ﻿namespace PartyInvites.Controllers
 {
+    using System;
+    using System.Linq;
+
     using Microsoft.AspNetCore.Mvc;
+
+    using PartyInvites.Models;
 
     public class HomeController : Controller
     {
+        public ViewResult Index()
+        {
+            ViewBag.Greeting = DateTime.Now.Hour < 12 ? "Morning" : "Afternoon";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+
+                return View("Thanks", guestResponse);
+            }
+
+            return View();
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(response => response.WillAttend == true));
+        }
     }
 }
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,16 @@
+﻿namespace PartyInvites.Models
+{
+    using System.Collections.Generic;
+
+    public static class Repository
+    {
+        private static readonly List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
