3d2e3f84fa151aad428a8146b08b8e5a4b0053b0
Aleksbgbg <kozichka01@abv.bg>
2018-11-04 19:32:35 +0000

    [SessionExtensions] Add extensions that serialize and deserialize data as JSON from session.

---
+++ Sports Store/SportsStore/Extensions/SessionExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsStore.Extensions
+{
+    using Microsoft.AspNetCore.Http;
+
+    using Newtonsoft.Json;
+
+    public static class SessionExtensions
+    {
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            string sessionData = session.GetString(key);
+
+            return sessionData == null ? default : JsonConvert.DeserializeObject<T>(sessionData);
+        }
+
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+    }
+}
