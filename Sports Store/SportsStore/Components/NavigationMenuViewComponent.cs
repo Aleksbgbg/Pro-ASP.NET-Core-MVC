66550fa3028be3c44787c8ea9e05f091233fcb70
Aleksbgbg <kozichka01@abv.bg>
2018-11-04 16:02:39 +0000

    [NavigationMenuViewComponent] Add component for navigation menu, which displays all distinct product categories.

---
+++ Sports Store/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿namespace SportsStore.Components
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Models;
+
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private readonly IProductRepository _productRepository;
+
+        public NavigationMenuViewComponent(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_productRepository.Products
+                                          .Select(product => product.Category)
+                                          .Distinct()
+                                          .OrderBy(category => category));
+        }
+    }
+}
