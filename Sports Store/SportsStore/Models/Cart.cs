e93c07f6ffc8bd4a14727181b2f4238eab04bbbc
Aleksbgbg <kozichka01@abv.bg>
2018-11-04 18:11:02 +0000

    [Cart] Add CartLine and Cart models to manage a cart of shopping items.

---
+++ Sports Store/SportsStore/Models/Cart.cs
@@ -0,0 +1,42 @@
+﻿namespace SportsStore.Models
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Cart
+    {
+        private readonly List<CartLine> _cartLines = new List<CartLine>();
+
+        public virtual IEnumerable<CartLine> CartLines => _cartLines;
+
+        public virtual decimal TotalCost => _cartLines.Sum(cartLine => cartLine.Product.Price * cartLine.Quantity);
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine targetLine = _cartLines.FirstOrDefault(cartLine => cartLine.Product.Id == product.Id);
+
+            if (targetLine == null)
+            {
+                _cartLines.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                targetLine.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product)
+        {
+            _cartLines.RemoveAll(cartLine => cartLine.Product.Id == product.Id);
+        }
+
+        public virtual void Clear()
+        {
+            _cartLines.Clear();
+        }
+    }
+}
---
+++ Sports Store/SportsStore/Models/CartLine.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsStore.Models
+{
+    public class CartLine
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
