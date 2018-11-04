8c135a7ed6d1e6ffacee3f03881367cc4b561bb1
Aleksbgbg <kozichka01@abv.bg>
2018-11-04 13:43:23 +0000

    [ProductControllerTests] Add CanFilterProducts test, which checks product filtering by category is accurate.

--- Sports Store/SportsStore.Tests/Controllers/ProductControllerTests.cs
+++ Sports Store/SportsStore.Tests/Controllers/ProductControllerTests.cs
@@ -108,5 +108,56 @@
             Assert.Equal(5, pageInfo.ItemCount);
             Assert.Equal(2, pageInfo.TotalPages);
         }
+
+        [Fact]
+        public void CanFilterProducts()
+        {
+            // Arrange
+            Mock<IProductRepository> productRepositoryMock = new Mock<IProductRepository>();
+            productRepositoryMock.Setup(productRepository => productRepository.Products)
+                                 .Returns(new Product[]
+                                 {
+                                     new Product
+                                     {
+                                         Id = 1,
+                                         Name = "P1",
+                                         Category = "Cat1"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 2,
+                                         Name = "P2",
+                                         Category = "Cat2"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 3,
+                                         Name = "P3",
+                                         Category = "Cat1"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 4,
+                                         Name = "P4",
+                                         Category = "Cat2"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 5,
+                                         Name = "P5",
+                                         Category = "Cat3"
+                                     }
+                                 });
+
+            ProductController controller = new ProductController(productRepositoryMock.Object);
+
+            // Act
+            Product[] result = ((ProductsList)controller.List("Cat2").ViewData.Model).Products.ToArray();
+
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
     }
 }
