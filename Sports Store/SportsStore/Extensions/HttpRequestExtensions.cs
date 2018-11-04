49f36131416296edaf087c313429f59da1085e59
Aleksbgbg <kozichka01@abv.bg>
2018-11-04 18:51:41 +0000

    [HttpRequestExtensions] Add extension method for HttpRequest that returns the path and query string (if it exists) of a HttpRequest.

---
+++ Sports Store/SportsStore/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsStore.Extensions
+{
+    using Microsoft.AspNetCore.Http;
+
+    public static class HttpRequestExtensions
+    {
+        public static string PathAndQuery(this HttpRequest request)
+        {
+            return request.QueryString.HasValue ? string.Concat(request.Path, request.QueryString) : request.Path.ToString();
+        }
+    }
+}
