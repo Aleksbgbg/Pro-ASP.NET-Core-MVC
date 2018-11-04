1de8175b6471a49fc5e122fe95ca8cc4b32612d4
Aleksbgbg <kozichka01@abv.bg>
2018-11-04 11:22:05 +0000

    [PagingInfo] Add PagingInfo view model, with a using in ViewImports for the ViewModels namespace.

---
+++ Sports Store/SportsStore/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace SportsStore.Models.ViewModels
+{
+    using System;
+
+    public class PagingInfo
+    {
+        public int ItemCount { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling((double)ItemCount / ItemsPerPage);
+    }
+}
