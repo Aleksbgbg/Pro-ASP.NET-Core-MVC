c2ba8e8d35fc3c1c67da50ae3582e212a4209618
Aleksbgbg <kozichka01@abv.bg>
2018-11-04 11:52:58 +0000

    [ProductControllerTests] Add extra test to check if ProductController returns correct pagination view model. Edit CanPaginate test to use correct return type for view data model.

--- Sports Store/SportsStore.Tests/Controllers/ProductControllerTests.cs
+++ Sports Store/SportsStore.Tests/Controllers/ProductControllerTests.cs
@@ -1,12 +1,12 @@
 ﻿namespace SportsStore.Tests.Controllers
 {
-    using System.Collections.Generic;
     using System.Linq;
 
     using Moq;
 
     using SportsStore.Controllers;
     using SportsStore.Models;
+    using SportsStore.Models.ViewModels;
 
     using Xunit;
 
@@ -51,13 +51,62 @@
             ProductController controller = new ProductController(productRepositoryMock.Object);
 
             // Act
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).ViewData.Model;
+            ProductsList result = (ProductsList)controller.List(2).ViewData.Model;
 
             // Assert
-            Product[] products = result.ToArray();
+            Product[] products = result.Products.ToArray();
 
             Assert.True(products.Length == 1);
             Assert.Equal("P5", products[0].Name);
+        }
+
+        [Fact]
+        public void CanSendPaginationViewModel()
+        {
+            // Arrange
+            Mock<IProductRepository> productRepositoryMock = new Mock<IProductRepository>();
+            productRepositoryMock.Setup(productRepository => productRepository.Products)
+                                 .Returns(new Product[]
+                                 {
+                                         new Product
+                                         {
+                                                 Id = 1,
+                                                 Name = "P1"
+                                         },
+                                         new Product
+                                         {
+                                                 Id = 2,
+                                                 Name = "P2"
+                                         },
+                                         new Product
+                                         {
+                                                 Id = 3,
+                                                 Name = "P3"
+                                         },
+                                         new Product
+                                         {
+                                                 Id = 4,
+                                                 Name = "P4"
+                                         },
+                                         new Product
+                                         {
+                                                 Id = 5,
+                                                 Name = "P5"
+                                         }
+                                 });
+
+            ProductController controller = new ProductController(productRepositoryMock.Object);
+
+            // Act
+            ProductsList result = (ProductsList)controller.List(2).ViewData.Model;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(4, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.ItemCount);
+            Assert.Equal(2, pageInfo.TotalPages);
         }
     }
 }
