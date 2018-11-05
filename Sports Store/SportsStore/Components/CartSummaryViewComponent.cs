ef6ce025cc4b1967a58e407a205f77c69b384ee2
Aleksbgbg <kozichka01@abv.bg>
2018-11-05 19:01:49 +0000

    [CartSummaryViewComponent] Add component for the cart summary, including a button which takes the user to their cart.

---
+++ Sports Store/SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsStore.Components
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Models;
+
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly Cart _cart;
+
+        public CartSummaryViewComponent(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_cart);
+        }
+    }
+}
