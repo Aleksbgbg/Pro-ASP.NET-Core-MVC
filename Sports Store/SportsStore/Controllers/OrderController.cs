735ef80a45add7925bb1177bfa2f1a146c077deb
Aleksbgbg <kozichka01@abv.bg>
2018-11-05 20:12:40 +0000

    [OrderController] Add a controller which simply displays the order form view.

---
+++ Sports Store/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsStore.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Models;
+
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout()
+        {
+            return View(new Order());
+        }
+    }
+}
