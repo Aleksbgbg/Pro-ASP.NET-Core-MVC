1baef81c552ac2bb8de9c9ade84f0a351254519e
Aleksbgbg <kozichka01@abv.bg>
2018-11-05 12:29:26 +0000

    [SessionCart] Add Cart sub-class which is aware of its session, and modifies it appropriately.

---
+++ Sports Store/SportsStore/Models/SessionCart.cs
@@ -0,0 +1,45 @@
+﻿namespace SportsStore.Models
+{
+    using System;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using Newtonsoft.Json;
+
+    using SportsStore.Extensions;
+
+    public class SessionCart : Cart
+    {
+        [JsonIgnore]
+        private ISession Session { get; set; }
+
+        public static SessionCart Make(IServiceProvider serviceProvider)
+        {
+            ISession session = serviceProvider.GetRequiredService<IHttpContextAccessor>().HttpContext.Session;
+
+            SessionCart sessionCart = session?.GetJson<SessionCart>("Cart") ?? new SessionCart();
+            sessionCart.Session = session;
+
+            return sessionCart;
+        }
+
+        public override void AddItem(Product product, int quantity)
+        {
+            base.AddItem(product, quantity);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void RemoveLine(Product product)
+        {
+            base.RemoveLine(product);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            Session.Remove("Cart");
+        }
+    }
+}
