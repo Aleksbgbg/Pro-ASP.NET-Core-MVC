4d16ecc109ff46614a8e77f7da3cbe2568087e2e
Aleksbgbg <kozichka01@abv.bg>
2018-11-07 20:11:37 +0000

    [AdminControllerTests] Test index method on AdminController.

---
+++ Sports Store/SportsStore.Tests/Controllers/AdminControllerTests.cs
@@ -0,0 +1,59 @@
+﻿namespace SportsStore.Tests.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Moq;
+
+    using SportsStore.Controllers;
+    using SportsStore.Models;
+
+    using Xunit;
+
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void IndexContainsAllProducts()
+        {
+            // Arrange
+            Mock<IProductRepository> productRepositoryMock = new Mock<IProductRepository>();
+            productRepositoryMock.Setup(productRepository => productRepository.Products)
+                                 .Returns(new Product[]
+                                 {
+                                     new Product
+                                     {
+                                         Id = 1,
+                                         Name = "P1"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 2,
+                                         Name = "P2"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 3,
+                                         Name = "P3"
+                                     }
+                                 }.AsQueryable());
+
+            AdminController controller = new AdminController(productRepositoryMock.Object);
+
+            // Act
+            Product[] result = GetViewModel<IEnumerable<Product>>(controller.Index())?.ToArray();
+
+            // Assert
+            Assert.Equal(3, result.Length);
+            Assert.Equal("P1", result[0].Name);
+            Assert.Equal("P2", result[1].Name);
+            Assert.Equal("P3", result[2].Name);
+        }
+
+        private static T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+}
