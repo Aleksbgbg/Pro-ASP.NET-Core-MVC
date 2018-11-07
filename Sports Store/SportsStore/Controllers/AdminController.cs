107403880f414da00c8279aaeed531864bf83dde
Aleksbgbg <kozichka01@abv.bg>
2018-11-07 20:11:22 +0000

    [AdminController] Add controller for administration and website management.

---
+++ Sports Store/SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsStore.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Models;
+
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public AdminController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public ViewResult Index()
+        {
+            return View(_productRepository.Products);
+        }
+    }
+}
