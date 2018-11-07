84717b825a9cc9a8eda30b59204f76c1ec7343fb
Aleksbgbg <kozichka01@abv.bg>
2018-11-07 20:25:58 +0000

    [AdminControllerTests] Add unit tests for AdminController Edit action, checking whether products can be edited accurately.

--- Sports Store/SportsStore.Tests/Controllers/AdminControllerTests.cs
+++ Sports Store/SportsStore.Tests/Controllers/AdminControllerTests.cs
@@ -51,6 +51,78 @@
             Assert.Equal("P3", result[2].Name);
         }
 
+        [Fact]
+        public void CanEditProduct()
+        {
+            // Arrange
+            Mock<IProductRepository> productRepositoryMock = new Mock<IProductRepository>();
+            productRepositoryMock.Setup(productRepository => productRepository.Products)
+                                 .Returns(new Product[]
+                                 {
+                                     new Product
+                                     {
+                                          Id = 1,
+                                          Name = "P1"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 2,
+                                         Name = "P2"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 3,
+                                         Name = "P3"
+                                     }
+                                 }.AsQueryable());
+
+            AdminController controller = new AdminController(productRepositoryMock.Object);
+
+            // Act
+            Product p1 = GetViewModel<Product>(controller.Edit(1));
+            Product p2 = GetViewModel<Product>(controller.Edit(2));
+            Product p3 = GetViewModel<Product>(controller.Edit(3));
+
+            // Assert
+            Assert.Equal(1, p1.Id);
+            Assert.Equal(2, p2.Id);
+            Assert.Equal(3, p3.Id);
+        }
+
+        [Fact]
+        public void CannotEditNonExistentProduct()
+        {
+            // Arrange
+            Mock<IProductRepository> productRepositoryMock = new Mock<IProductRepository>();
+            productRepositoryMock.Setup(productRepository => productRepository.Products)
+                                 .Returns(new Product[]
+                                 {
+                                     new Product
+                                     {
+                                         Id = 1,
+                                         Name = "P1"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 2,
+                                         Name = "P2"
+                                     },
+                                     new Product
+                                     {
+                                         Id = 3,
+                                         Name = "P3"
+                                     }
+                                 }.AsQueryable());
+
+            AdminController controller = new AdminController(productRepositoryMock.Object);
+
+            // Act
+            Product result = GetViewModel<Product>(controller.Edit(4));
+
+            // Assert
+            Assert.Null(result);
+        }
+
         private static T GetViewModel<T>(IActionResult result) where T : class
         {
             return (result as ViewResult)?.ViewData.Model as T;
