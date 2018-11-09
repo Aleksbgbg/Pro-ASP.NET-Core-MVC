ad58be31a6009bd0e30e37607959fe42dbcf6c3d
Aleksbgbg <kozichka01@abv.bg>
2018-11-09 21:51:10 +0000

    [Product] Add validation attributes.

--- Sports Store/SportsStore/Models/Product.cs
+++ Sports Store/SportsStore/Models/Product.cs
@@ -1,15 +1,22 @@
 ﻿namespace SportsStore.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Product
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Please enter a category")]
         public string Category { get; set; }
     }
 }
