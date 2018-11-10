2891a92a96a5a1b5ac88161dee35fdc6090edecb
Aleksbgbg <kozichka01@abv.bg>
2018-11-10 18:17:42 +0000

    [ErrorController] Add controller with Error action which displays an error view.

---
+++ Sports Store/SportsStore/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsStore.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ErrorController : Controller
+    {
+        public ViewResult Error()
+        {
+            return View();
+        }
+    }
+}
