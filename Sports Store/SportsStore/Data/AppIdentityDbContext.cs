75a0f770f6220308921090d65cb67e0facbb7e8e
Aleksbgbg <kozichka01@abv.bg>
2018-11-10 15:23:01 +0000

    [AppIdentityDbContext] Add database context for storing identities.

---
+++ Sports Store/SportsStore/Data/AppIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsStore.Data
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
