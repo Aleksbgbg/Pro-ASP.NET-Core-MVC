7a17f269dd44d1166db9055db06bcb71da577b3c
Aleksbgbg <kozichka01@abv.bg>
2018-11-10 15:47:15 +0000

    [AccountController] Add controller which manages login and logout procedures.

---
+++ Sports Store/SportsStore/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿namespace SportsStore.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    using SportsStore.Models.ViewModels;
+
+    using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        public ViewResult Login(string returnUrl)
+        {
+            return View(new Login
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByNameAsync(login.Name);
+
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+
+                    SignInResult signInResult = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+
+                    if (signInResult.Succeeded)
+                    {
+                        return Redirect(login.ReturnUrl ?? "/Admin/Index");
+                    }
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid name or password");
+
+            return View(login);
+        }
+
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await _signInManager.SignOutAsync();
+
+            return Redirect(returnUrl);
+        }
+    }
+}
