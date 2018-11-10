05c22a7213196266934f247244945dfbe7678935
Aleksbgbg <kozichka01@abv.bg>
2018-11-10 15:40:07 +0000

    [Login] Add ViewModel for login data (name and password).

---
+++ Sports Store/SportsStore/Models/ViewModels/Login.cs
@@ -0,0 +1,16 @@
+﻿namespace SportsStore.Models.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Login
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
