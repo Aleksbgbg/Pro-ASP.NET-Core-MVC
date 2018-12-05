5c7e4a2c8dee6f23e75ca6ff862d38306cb7a819
Aleksbgbg <kozichka01@abv.bg>
2018-12-05 21:12:14 +0000

    [UrlsAndRouting] Add UrlsAndRouting project.

---
+++ UrlsAndRouting/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace UrlsAndRouting
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                          .UseStartup<Startup>();
+        }
+    }
+}
