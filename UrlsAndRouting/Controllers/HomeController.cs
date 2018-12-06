699d2b5294635398f4ef9a592704daa8c58a601d
Aleksbgbg <kozichka01@abv.bg>
2018-12-06 20:41:36 +0000

    [HomeController] Add several more routes for testing.

--- UrlsAndRouting/Controllers/HomeController.cs
+++ UrlsAndRouting/Controllers/HomeController.cs
@@ -10,5 +10,23 @@
         {
             return View("Target", new Target(nameof(HomeController), nameof(Index)));
         }
+
+        public ViewResult SomeId(int id)
+        {
+            Target target = new Target(nameof(HomeController), nameof(SomeId));
+
+            target.Data[nameof(id)] = id;
+
+            return View("Target", target);
+        }
+
+        public ViewResult VariableLength()
+        {
+            Target target = new Target(nameof(HomeController), nameof(VariableLength));
+
+            target.Data["catchall"] = RouteData.Values["catchall"] ?? "<no value>";
+
+            return View("Target", target);
+        }
     }
 }
