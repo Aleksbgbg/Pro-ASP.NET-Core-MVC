9d761199a8604cc8f890ba8a43e952875fc2ec1d
Aleksbgbg <kozichka01@abv.bg>
2018-12-06 20:42:03 +0000

    [ExactValueConstraint] Add URL route constraint which ensures the route matches an exact string value.

---
+++ UrlsAndRouting/Constraints/ExactValueConstraint.cs
@@ -0,0 +1,22 @@
+﻿namespace UrlsAndRouting.Constraints
+{
+    using System;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Routing;
+
+    public class ExactValueConstraint : IRouteConstraint
+    {
+        private readonly string _value;
+
+        public ExactValueConstraint(string value)
+        {
+            _value = value;
+        }
+
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return values[routeKey]?.ToString() == _value;
+        }
+    }
+}
