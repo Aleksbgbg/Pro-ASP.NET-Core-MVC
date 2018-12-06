59b8e080a7bd4236f66268bbc1b767ea49266a08
Aleksbgbg <kozichka01@abv.bg>
2018-12-06 20:42:16 +0000

    [Startup] Configure routes via RouteBuilder.

--- UrlsAndRouting/Startup.cs
+++ UrlsAndRouting/Startup.cs
@@ -2,12 +2,20 @@
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Routing;
     using Microsoft.Extensions.DependencyInjection;
+
+    using UrlsAndRouting.Constraints;
 
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RouteOptions>(options =>
+            {
+                options.ConstraintMap.Add("exactValue", typeof(ExactValueConstraint));
+            });
+
             services.AddMvc();
         }
 
@@ -19,7 +27,18 @@
             }
 
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+
+            app.UseMvc(routeBuilder =>
+            {
+                routeBuilder.MapRoute(name: "constraint",
+                                      template: "{controller:regex(^H)}/{action:exactValue(SomeId)}/{id:int}");
+
+                routeBuilder.MapRoute(name: "catchall",
+                                      template: "{controller=Home}/{action=VariableLength}/{*catchall}");
+
+                routeBuilder.MapRoute(name: "default",
+                                      template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
